dc0c29b5994a82a7be809111da17d9fe803ce48b
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-08-28 14:01:45 +0400

    Updated for Web scrapping 🟥

--- Video Processing/YT_video_clip_analysing.py
+++ Video Processing/YT_video_clip_analysing.py
@@ -1,45 +1,25 @@
-from googleapiclient.discovery import build
-
-# Replace with your API key
-API_KEY = 'AIzaSyBFhF1HsjPPikz2bin56-4x3yo6hArIhzI'
-
-# Create a YouTube API client
-youtube = build('youtube', 'v3', developerKey=API_KEY)
+from rich.traceback import install
+install(show_locals=True)
+from selenium import webdriver
 
 # Video ID of the YouTube video
 video_id = 'r5NQecwZs1A&ab'
 
-# Get video statistics (view count over time)
-video_stats_response = youtube.videos().list(
-    part='statistics',
-    id=video_id
-).execute()
+video_url = f'https://www.youtube.com/watch?v={video_id}'
+# Set up the WebDriver
+driver = webdriver.Chrome()  # You'll need to have ChromeDriver installed
 
-view_count_data = video_stats_response['items'][0]['statistics']['viewCount']
+# Open the video URL
+driver.get(video_url)
 
-# Convert view count data to a list of integers
-view_count_list = [int(view_count) for view_count in view_count_data.split(',')]
+# Find the element that contains the view count
+view_count_element = driver.find_element_by_xpath('//*[@class="view-count"]')
 
-# Calculate increases in view count for each interval
-view_count_increases = [view_count_list[i+1] - view_count_list[i] for i in range(len(view_count_list)-1)]
+if view_count_element:
+    view_count = view_count_element.text
+    print(f"View Count: {view_count}")
+else:
+    print("View count element not found on the page.")
 
-# Define the length of time intervals in seconds (adjust as needed)
-interval_length = 60  # 1 minute intervals
-
-# Analyze view count increases and identify most viewed sections
-most_viewed_sections = []
-for i, increase in enumerate(view_count_increases):
-    start_time = i * interval_length
-    end_time = (i + 1) * interval_length
-    most_viewed_sections.append((start_time, end_time, increase))
-
-# Sort sections by view count increase in descending order
-most_viewed_sections.sort(key=lambda x: x[2], reverse=True)
-
-# Print the most viewed sections
-print(f"Most Viewed Sections of '{video_id}':")
-for section in most_viewed_sections:
-    start_time = section[0]
-    end_time = section[1]
-    increase = section[2]
-    print(f"Time: {start_time}-{end_time} seconds | Increase in Views: {increase}")
+# Close the browser window
+driver.quit()
